fbea39137989a7b1a8e2edc2600a1a35d4dc5e16
Ran Yan <ranyan@0587379492.wireless.umich.net>
2021-11-14 16:24:25 -0500

    modify pd topic group file

--- pandas_notes/pd_topic_group2_3g_ranyan.py
+++ pandas_notes/pd_topic_group2_3g_ranyan.py
@@ -73,8 +73,6 @@ print(df.rolling(3, win_type='gaussian', min_periods=2,
 df.expanding(min_periods=2).sum()
 
 
-# In[19]:
-
 
 # modules: -------------------------------------------------------------------
 import numpy as np
@@ -94,24 +92,19 @@ import matplotlib.pyplot as plt
 import datetime
 
 
-# Ran Yan ranyan@umich.edu
+# ## Time Series 
+# **Ran Yan**
+# + [General Time Series Functions](#General-Time-Series-Functions) 
+# + [Convert to Timestamps](#Convert-to-Timestamps) 
+# + [Other Useful Time Series Functions](#Other-Useful-Time-Series-Functions) 
 
-# # Question 0
-# 
-# reference: https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html
 
-# ## Time Series
-# 
-# - pandas contains extensive capabilities and features for working with time series data for all domains. 
-# - can parse time series information in different formats and sources
-#     - see example 1 below
-# - can generate sequences of fixed-frequency dates and time spans
-#     - see example 2 below
-# - ca manipulating and converting date times with timezone information
-#     - see example 3 below
+# # Question 0 - Topics in Pandas
 
-# In[25]:
-
+# ##  General Time Series Functions
+# * pandas contains extensive capabilities and features for working with time series data for all domains.
+# * can parse time series information in different formats and sources
+# * can manipulating and converting date times with timezone information
 
 # example 1
 dti = pd.to_datetime(
@@ -119,17 +112,9 @@ dti = pd.to_datetime(
 )
 dti
 
-
-# In[29]:
-
-
 # example 2
 dti = pd.date_range("2000-01", periods=5, freq="D")
 dti
-
-
-# In[30]:
-
 
 # example 3
 dti = dti.tz_localize("UTC")
@@ -137,19 +122,15 @@ dti
 
 
 # ## Convert to Timestamps
-# 
-# - when convert to timestamps, convert series to series and convert list-like data to DatetimeIndex
-# - return a single timestamp if pass a string
-# - can use format argument to ensure specific parsing, here is an example below
-
-# In[31]:
+# * when convert to timestamps, convert series to series and convert list-like data to DatetimeIndex
+# * return a single timestamp if pass a string
+# * can use format argument to ensure specific parsing, here is an example below
 
 
 pd.to_datetime("2021/10/23", format="%Y/%m/%d")
 
 
-# ## Some useful Time Series Functions
-# 
+# ## Other Useful Time Series Functions
 # - `between_time` 
 #     - to select rows in data frame that are only between certain time range
 #     - here is an example below to select time from 9:00 to 10:00
@@ -158,23 +139,12 @@ pd.to_datetime("2021/10/23", format="%Y/%m/%d")
 # - `bdate_range`
 #     - create DatetimeIndex for in a range of business days
 
-# In[23]:
-
-
 rng = pd.date_range('2/3/2000', periods=24, freq='H')
 ts = pd.Series(np.random.randn(len(rng)), index=rng)
 ts.iloc[ts.index.indexer_between_time(datetime.time(9), datetime.time(10))]
 
-
-# In[33]:
-
-
 stamps = pd.date_range("2021-10-23 20:15:05", periods=2, freq="H")
 stamps
-
-
-# In[34]:
-
 
 start = datetime.datetime(2021, 10, 20)
 end = datetime.datetime(2021, 10, 23)
